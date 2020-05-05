40211f32f038d8488b99d8f5fca33a5ce2870c0f
dediacc <dedisuhaimiacc@gmail.com>
2020-05-05 13:52:47 +0700

    Updates

---
+++ precache-manifest.cb46aea0da1d33cbe585d813210d56c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15abf3a6b9cf357605c02ec9356d36a",
+    "url": "/tutor-management/index.html"
+  },
+  {
+    "revision": "2d9ff1c3deba7126b215",
+    "url": "/tutor-management/static/css/main.a485a674.chunk.css"
+  },
+  {
+    "revision": "47215bd0adb59ef7285e",
+    "url": "/tutor-management/static/js/2.7116fcf2.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/tutor-management/static/js/2.7116fcf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9ff1c3deba7126b215",
+    "url": "/tutor-management/static/js/main.d8ae307e.chunk.js"
+  },
+  {
+    "revision": "4dcd8cf5633e25c14312",
+    "url": "/tutor-management/static/js/runtime-main.5cadc3ac.js"
+  }
+]);
